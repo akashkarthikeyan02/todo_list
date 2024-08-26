686816ba32e65c927f6b0a829a45f9565c90f927
akashkarthikeyan02 <akashwb1b@gmail.com>
2024-08-26 13:46:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TodoWrapper } from './component/TodoWrapper'
+
+import './App.css'
+import TodoForm from './component/TodoForm'
+
+export const App = () => {
+  return (
+    <div className="App">
+    <TodoWrapper/>
+    </div>
+  )
+}
+export default App
